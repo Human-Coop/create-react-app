e9e0078d7c1219df3c7040dc9719a23b5b10c7be
AlexCat <eiseman.alex@gmail.com>
2023-04-10 17:41:27 -0500

    sandbox

--- src/App.js
+++ src/App.js
@@ -14,41 +14,15 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>May 4th 2024</h1>
       <h2>
-        Deployed with{' '}
-        <a
-          href="https://vercel.com/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          Vercel
-        </a>
-        !
+        A website with info about our wedding
       </h2>
       <p>
-        <a
-          href="https://github.com/vercel/vercel/tree/main/examples/create-react-app"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
+        List of pages and links coming soon
       </p>
       <br />
-      <h2>The date according to Go is:</h2>
+      <h2>Countdown till wedding:</h2>
       <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
