d8dc6f5e319b98b17982f772641f84e3a63f2c24
AlexCat <eiseman.alex@gmail.com>
2023-04-10 17:56:44 -0500

    restore date

--- api/date.go
+++ api/date.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	weddingTime := time.Date(2023,time.May, 4, 18, 0, 0, 0, time.UTC).Format(time.RFC850)
+	// weddingTime := time.Date(2023,time.May, 4, 18, 0, 0, 0, time.UTC).Format(time.RFC850)
 	currentTime := time.Now().Format(time.RFC850)
 	fmt.Fprintf(w, currentTime)
 }
