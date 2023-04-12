0bce0738bd09b8e2a0314604246b58b70bcacf0a
AlexCat <eiseman.alex@gmail.com>
2023-04-12 12:21:08 -0500

    test go date

--- api/date.go
+++ api/date.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	weddingTime = time.Date(2024, time.Month(5), 4, 18, 30, 0, 0, time.UTC)
+	weddingTime = time.Parse("2024-05-04")
 	currentTime := time.Now().Format(time.RFC850)
 	fmt.Fprintf(w, currentTime)
 }
