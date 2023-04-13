5069abe493a54062570f94ef3a6b43b48fad9325
AlexCat <95706155+Human-Coop@users.noreply.github.com>
2023-04-13 13:16:28 -0500

    test countdown

--- api/date.go
+++ api/date.go
@@ -9,6 +9,6 @@ import (
 func Handler(w http.ResponseWriter, r *http.Request) {
 	weddingTime := time.Date(2024, 05, 04, 18, 30, 00, 651387237, time.UTC)
 	diffTime := weddingTime.Sub(time.Now())
-	fmtTime := fmt.SPrint("%d Hours %d Minutes %d Seconds %d NanoSeconds", int64(diffTime.Hours()), int64(diffTime.Minutes()), int64(diffTime.Seconds()), int64(diffTime.Nanoseconds()))
+	fmtTime := fmt.SPrint(int64(diffTime.Hours()), " Hours ", int64(diffTime.Minutes()), " Minutes ", int64(diffTime.Seconds()), " Seconds ", int64(diffTime.Nanoseconds()), " NanoSeconds")
 	fmt.Fprintf(w, fmtTime)
 }
