6cba08a57f9b622bcf6adea3d3dd6a9c6b0fe984
AlexCat <95706155+Human-Coop@users.noreply.github.com>
2023-04-13 13:20:40 -0500

    restore

--- api/date.go
+++ api/date.go
@@ -9,6 +9,6 @@ import (
 func Handler(w http.ResponseWriter, r *http.Request) {
 	weddingTime := time.Date(2024, 05, 04, 18, 30, 00, 651387237, time.UTC)
 	diffTime := weddingTime.Sub(time.Now())
-	fmtTime := fmt.SPrint(int64(diffTime.Hours()), " Hours ", int64(diffTime.Minutes()), " Minutes ", int64(diffTime.Seconds()), " Seconds ", int64(diffTime.Nanoseconds()), " NanoSeconds")
+	fmtTime := fmt.Sprintf("%d Hours %d Minutes %d Seconds %d NanoSeconds", int64(diffTime.Hours()), int64(diffTime.Minutes()), int64(diffTime.Seconds()), int64(diffTime.Nanoseconds()))
 	fmt.Fprintf(w, fmtTime)
 }
