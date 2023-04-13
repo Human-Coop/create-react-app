cf53c002e48783601e7bc4d5dce5fce0e507d857
AlexCat <95706155+Human-Coop@users.noreply.github.com>
2023-04-13 11:52:47 -0500

    test date

--- api/date.go
+++ api/date.go
@@ -7,9 +7,9 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	currentTime := time.Now()
+	currentTime := time.Now().Format(time.RFC850)
 	weddingTime := time.Date(2024, 05, 04, 18, 30, 00, 651387237, time.UTC)
-	diffTime := weddingTime.Sub(currentTime) 
+	// diffTime := weddingTime.Sub(currentTime) 
 	// .Format(time.RFC850)
-	fmt.Fprintf(w, diffTime)
+	fmt.Fprintf(w, currentTime)
 }
