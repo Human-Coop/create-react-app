699f1b3a6104037abcaae29d2d39f3bbe067de22
AlexCat <95706155+Human-Coop@users.noreply.github.com>
2023-05-09 16:08:16 -0500

    use Local time

--- api/date.go
+++ api/date.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	weddingTime := time.Date(2024, 05, 04, 18, 30, 00, 651387237, time.UTC)
+	weddingTime := time.Date(2024, 05, 04, 18, 30, 00, 651387237, time.Local)
 	diffTime := weddingTime.Sub(time.Now())
 	totalHours := int64(diffTime.Hours())
 	totalMinutes := int64(diffTime.Minutes())
