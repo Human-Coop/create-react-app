504c51c92e0b84747848c12c0b0ff1a066b32769
AlexCat <95706155+Human-Coop@users.noreply.github.com>
2023-05-10 16:10:24 -0500

    add sleeping suggestions

--- src/App.js
+++ src/App.js
@@ -23,11 +23,20 @@ function App() {
         Wedding Events
       </h3>
       <ul>
-        <li>Crawfish Boil 5/3/2024 6:00pm</li>
-        <li>Wedding Ceremony 5/4/2024 6:00pm</li>
-        <li>Reception 5/4/2024 7:00pm</li>
-        <li>Lantern Parade to Afterpary 5/4/2024 9:00pm</li>
-        <li>Bagel Brunch 5/5/2024 10:00am</li>
+        <li>Crawfish Boil 5/3/2024 6:00pm <a>Details</a> | <a>RSVP</a></li>
+        <li>Wedding Ceremony 5/4/2024 6:00pm <a>Details</a> | <a>RSVP</a></li>
+        <li>Reception 5/4/2024 7:00pm <a>Details</a> | <a>RSVP</a></li>
+        <li>Lantern Parade to Afterpary 5/4/2024 9:00pm <a>Details</a></li>
+        <li>Bagel Brunch 5/5/2024 10:00am <a>Details</a> | <a>RSVP</a></li>
+      </ul>
+      <br />
+      <h3>
+        Sleeping Accomodation Suggestions
+      </h3>
+      <ul>
+        <li>State Park Cabins <a>Book</a></li>
+        <li>State Park Hotel <a>Book</a></li>
+        <li>Stake Park camping <a>Book</a></li>
       </ul>
       <br />
       <h2>Countdown:</h2>
