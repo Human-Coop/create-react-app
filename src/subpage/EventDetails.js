7f6e883911a890de591e9123b2051aff23e840d5
AlexCat <95706155+Human-Coop@users.noreply.github.com>
2023-05-13 18:03:45 -0500

    add description styling

--- src/subpage/EventDetails.js
+++ src/subpage/EventDetails.js
@@ -6,9 +6,9 @@ function EventDetails({ event }) {
   return (
     <div class="eventDetails">
         <img width="300" src={event.picture} />
-        <p>Time: {event.time}</p>
-        <p>Location: <a href={event.maplink}> {event.location}</a></p>
-        <p>{event.description}</p>
+        <h4>Time:</h4><p> {event.time}</p>
+        <h4>Location:</h4> <a href={event.maplink}> {event.location}</a>
+        <div>{event.description}</div>
     </div>
   )
 }
