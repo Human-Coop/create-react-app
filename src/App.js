00f84632cc6cad9c404c1199f92e0e1971ade370
AlexCat <95706155+Human-Coop@users.noreply.github.com>
2023-05-17 18:26:18 -0500

    update crawfish pic

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
     {
       name: "Crawfish Boil",
       id: "crawfishBoil",
-      picture:"https://www.cajungrocer.com/image/cache/wp/gj/articles/Live-Crawfish-1140x800.webp",
+      picture:"https://lh3.googleusercontent.com/pw/AJFCJaWQoouUU75Qv3awjdt-zjd4lJxDMiT5tUYDF2xzN70bufH2b4pLyASc3Ps_E5AsRkyNo6CLhnHyV6VvKyiagTy3tBKTwhV7-daw7LjVmQi7QnxS0ggWCIBL5bYPVhVHwZG-6oPWoqDF63OHZSiHsuIZ=w177-h237-s-no?authuser=0",
       time: "6:00pm May 3rd 2024",
       maplink: "https://www.google.com/maps/dir//Eagle+Cottages+at+Gulf+State+Park,+1+Bald+Eagle+Lane,+Gulf+Shores,+AL+36542/@30.2663425,-87.6659432,15z/data=!4m9!4m8!1m0!1m5!1m1!1s0x889a058b1438f8dd:0x51a40a2c356d042a!2m2!1d-87.6659432!2d30.2663425!3e0",
       location: "Eagle Cottages",
