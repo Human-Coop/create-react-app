72cc2b93be99b903ac978db78a47aa8b5e5f3439
AlexCat <95706155+Human-Coop@users.noreply.github.com>
2023-05-17 18:11:56 -0500

    update general info

--- src/App.js
+++ src/App.js
@@ -219,10 +219,10 @@ function App() {
       <h4>
         You're invited to a low key celebration of our love at the beach in Gulf Shores.
         You can expect some relaxing beach time with some buffet style food and booze at our events.
-        If you can't make it to our celebration, or it is too logistically inconvenient for you, 
-        please rsvp no to the reception and make plans to celebrate with us at another place and time.
-        We'll post more information here as we get closer to our event.
+        If you can't make it to our celebration, please RSVP no to the reception and make plans 
+        to celebrate with us another time. We'll post more information here as we get closer to our event.
       </h4>
+      <br></br>
       <h3>
         Wedding Events
       </h3>
