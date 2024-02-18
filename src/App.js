248a517c752ace226f047d7c07c1d97cb97b6b44
Regina Sullivan <eiseman.alex@gmail.com>
2024-02-18 15:28:15 -0600

    syntax test3

--- src/App.js
+++ src/App.js
@@ -215,18 +215,7 @@ function App() {
 
 
   function jgetCountdown(){
-      // Set the date we're counting down to
-      var countDownDate = new Date("May 4, 2024 19:00:00").getTime();
-      // get date
-      var now = new Date().getTime();
-      // Find the distance between now and the count down date
-      var distance = countDownDate - now;
-      // Time calculations for days, hours, minutes and seconds
-      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      var str_date = days.toString() + "d " + hours.toString() + "h "+ minutes.toString() + "m " + seconds.toString() + "s ";
+
       return str_date;
       // if (distance > 0) {
       // } else{
@@ -235,7 +224,21 @@ function App() {
   }
 
   // Update the count down every 1 second
-  let jcountdown = setInterval(function(){document.getElementById("countdown").innerHTML = jgetcountdown();}, 10000);
+  let jcountdown = setInterval(function(){
+    // Set the date we're counting down to
+    var countDownDate = new Date("May 4, 2024 19:00:00").getTime();
+    // get date
+    var now = new Date().getTime();
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var str_date = days.toString() + "d " + hours.toString() + "h "+ minutes.toString() + "m " + seconds.toString() + "s ";
+    document.getElementById("countdown").innerHTML = str_date;
+  }, 10000);
 
   useEffect(() => {
     async function getCountdown() {
