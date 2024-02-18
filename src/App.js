0f7b79cee4b1e6204e6ac6cb00eaa2f3f40cbcb7
Regina Sullivan <eiseman.alex@gmail.com>
2024-02-18 14:52:59 -0600

    syntax

--- src/App.js
+++ src/App.js
@@ -228,7 +228,7 @@ function App() {
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
       if (distance > 0) {
-        str_date = days.toString() + "d " + hours.toString() + "h "+ minutes.toString() + "m " + seconds.toString() + "s "
+        var str_date = days.toString() + "d " + hours.toString() + "h "+ minutes.toString() + "m " + seconds.toString() + "s "
         return str_date;
       } else{
         return "We Married"
