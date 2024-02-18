1a1b6340532f4b29ceea0601b8d3441092a141d1
Regina Sullivan <eiseman.alex@gmail.com>
2024-02-18 14:31:26 -0600

    add javascript timer

--- src/App.js
+++ src/App.js
@@ -227,8 +227,8 @@ function App() {
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-      if (distance < 0) {
-        return days + "d " + hours + "h "+ minutes + "m " + seconds + "s ";
+      if (distance > 0) {
+        return (days + "d " + hours + "h "+ minutes + "m " + seconds + "s ");
       } else{
         return "We Married"
       }
