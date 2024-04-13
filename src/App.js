1014bcb4d51efe07b1cb635115c09763bdc8c5dd
Regina Sullivan <eiseman.alex@gmail.com>
2024-04-13 16:13:36 -0500

    add map

--- src/App.js
+++ src/App.js
@@ -243,7 +243,7 @@ function App() {
       <br></br>
       {/* <h3><a onClick={() => clickForm(events[3])} >Please RSVP Here by March 4th</a></h3>
       <div>{ events[3].rsvpState && <EventForm event={events[3]} />}</div> */}
-      <img width="100%" src="https://drive.google.com/file/d/1HplJARz_4H74d7D4am9M72957OIwNuSl/preview"/>
+      <img width="100%" src="https://lh3.googleusercontent.com/pw/AP1GczMlwjW2c-wU_n2Fj0V8TEROOMnvkxq8wkNudNM06mqeWSjSSuBAdQfowvr5EeI9ABR-mr5RIlDyRACYDLTSgQBvZBInvbcbyknuppAbTr7gX1OOInPy_h2MU6N2I6QRtr1ptjnsC2az7MWyuzVFdfHuyw=w1130-h869-s-no-gm?authuser=0"/>
       <br></br>
       <a href="https://open.spotify.com/playlist/5RAiSe6SbaCCgsGgrcjleD?si=x1Vd6ReWRQil8bxoYZScwQ&pt=54ee4d5cd13e724f39db3d0607437868&pi=u-V9Te1T8cQEWM">Contribute to our Spotify wedding playlist</a>
       <br />
